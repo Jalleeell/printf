54c80117528cb8a8d055750ae7c821befe00f510
jalleeell <akljalil026@gmail.com>
2023-07-24 12:06:14 -0700

    ptr fun

---
+++ tests/_ptr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/********still working on it*******/
+
+/**
+ * give_address - Prints pointer address
+ * @ptr: pointer to print address of
+ *
+ * Return: len
+ */
+
+int give_address(unsigned long long ptr)
+{
+	int len = 0;
+
+	len += print_string("0x");
+	if (ptr == 0)
+	{
+		len += _putchar('0');
+	}
+	else
+	{
+		print_hex1(ptr, &len);
+		len += give_len(ptr);
+	}
+	return (len);
+}
