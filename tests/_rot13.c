3464b287b6fe032fccda1b4ad7191c72c4964a50
jalleeell <akljalil026@gmail.com>
2023-07-25 09:25:19 -0700

    add rot13

---
+++ tests/_rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+char* _rot13(const char *s)
+{
+    char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+    char *t = str_dup(s);
+    if (!t)
+    {
+        return (NULL);
+    }
+
+    for (int i = 0; t[i] != '\0'; i++)
+    {
+        for (int j = 0; j < 52; j++)
+        {
+            if (t[i] == alpha[j])
+            {
+                t[i] = rot[j];
+                break;
+            }
+        }
+    }
+
+    return (t);
+}
